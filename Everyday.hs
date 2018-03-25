b679395b464136cd01acf0a98e126b84fdd9ddf4
Laurence Emms <whatthefunctionalblog@gmail.com>
2018-03-25 01:47:05 -0700

    Added more example code.

--- Everyday.hs
+++ Everyday.hs
@@ -4,17 +4,74 @@
 
 import System.IO --Required for hFlush, withFile
 import System.Environment --Requirment for getArgs
+import Data.List --Requirement for intercalate
+
+orderType :: String -> IO ()
+orderType menuItem
+    | menuItem == "Ice cream" || menuItem == "Gulab Jamun" = putStr "Dessert ordered"
+    | menuItem == "Pakora" || menuItem == "Mini samosas" = putStr "Starter ordered"
+    | otherwise = return ()
+
+isVIP :: Int -> IO ()
+isVIP rewardPoints
+    = if rewardPoints > 50
+      then putStr "VIP member"
+      else return ()
+
+computeCustomerDiscount :: Int -> Int -> Float
+computeCustomerDiscount price rewardPoints
+    | rewardPoints > 5 = (fromIntegral price) * discountRate * tax
+    | otherwise = (fromIntegral price) * tax
+        where discountRate = 0.15
+              tax = 0.2
+
+printCustomerRecord :: (String, String, Int) -> IO ()
+printCustomerRecord (name, menuItem, rewardPoints)
+    = let stringRecord = show (name, menuItem, rewardPoints) in
+          putStr stringRecord
+
+findItemCost :: String -> [(String, Float)] -> Maybe Float
+findItemCost _ [] = Nothing
+findItemCost itemName ((item, cost) : menuItems)
+    | itemName == item = Just cost
+    | otherwise = findItemCost itemName menuItems
+
+getTotalCustomerSpent :: [(String, String, Int)] -> [(String, Float)] -> Float
+getTotalCustomerSpent [] _ = 0.0
+getTotalCustomerSpent ((name, menuItem, rewardPoints) : records) menuItems
+    = let menuItemCost = (findItemCost menuItem menuItems) in
+          case menuItemCost of
+          Nothing -> getTotalCustomerSpent records menuItems
+          Just cost -> cost + (getTotalCustomerSpent records menuItems)
+              
+printCustomerMenuItems :: String -> [(String, String, Int)] -> [String] -> IO ()
+printCustomerMenuItems customerName [] stack = putStr (intercalate " " stack) --The final record was processed
+printCustomerMenuItems customerName ((name, menuItem, _) : records) stack
+    | customerName == name = printCustomerMenuItems customerName records (menuItem : stack) --Push menu item on stack
+    | otherwise = printCustomerMenuItems customerName records stack
+
+--generateRecord :: IO (String, String, Int)
+--generateRecord = putStr "Enter customer name:\n" >>
+                 --hFlush stdout >>
+                 --getLine >>= (\name ->
+                 --putStr "Enter menu item:\n" >>
+                 --hFlush stdout >>
+                 --getLine >>= (\menuItem ->
+                 --putStr "Enter number of reward points:\n" >>
+                 --hFlush stdout >>
+                 --readLn >>= (\rewardPoints -> return (name, menuItem, rewardPoints))))
 
 generateRecord :: IO (String, String, Int)
-generateRecord = putStr "Enter customer name:\n" >>
-                 hFlush stdout >>
-                 getLine >>= (\name ->
-                 putStr "Enter menu item:\n" >>
-                 hFlush stdout >>
-                 getLine >>= (\menuItem ->
-                 putStr "Enter number of reward points:\n" >>
-                 hFlush stdout >>
-                 readLn >>= (\rewardPoints -> return (name, menuItem, rewardPoints))))
+generateRecord = do putStr "Enter customer name:\n"
+                    hFlush stdout
+                    name <- getLine
+                    putStr "Enter menu item:\n"
+                    hFlush stdout
+                    menuItem <- getLine
+                    putStr "Enter number of reward points:\n"
+                    hFlush stdout
+                    rewardPoints <- readLn
+                    return (name, menuItem, rewardPoints)
 
 printRecord :: (String, String, Int) -> IO ()
 printRecord (name, menuItem, rewardPoints) = putStr "Customer: " >>
